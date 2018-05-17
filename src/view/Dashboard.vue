5d4c3b371dc8c74f98829d37d9e8be2e024c0777
jihonghai <honghai_ji@163.com>
2018-05-17 17:32:56 +0800

    delete test code

--- src/view/Dashboard.vue
+++ src/view/Dashboard.vue
@@ -1,47 +1,18 @@
 <template>
-  <section class="wrapper" @click="login">
-    {{ loginMsg }}
+  <section class="wrapper">
   </section>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
-import { Getter, Action } from 'vuex-class'
-import { LoginRequest } from '@/types'
+import { Vue, Component } from 'vue-property-decorator'
+import { Getter } from 'vuex-class'
 
 @Component({})
 export default class Dashboard extends Vue {
-  @Prop({default: 'id'})
-  id: string
-
-  // data
-  msg: string = 'Welcome to Your Vue.js App DashBoard'
-
   @Getter('isLoggedIn') isLoggedIn: boolean
-
-  @Action('login') loginAction: Function
-
-  // computed
-  get loginMsg () {
-    return this.msg + this.isLoggedIn + `: ${this.id}`
-  }
-
-  @Watch('loginMsg')
-  onChildChanged (val: string, oldVal: string) {
-    console.log(val)
-    console.log(oldVal)
-  }
-
-  login () {
-    const params: LoginRequest = {
-      userName: '',
-      userPwd: ''
-    }
-    this.loginAction(params)
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="less">
 </style>
